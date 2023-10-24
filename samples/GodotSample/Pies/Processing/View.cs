46f23ce73109a89dc7ed84869229af1796033ede
Ilnaz Khasanov <ilnaz.khasanoff2003@gmail.com>
2023-10-24 11:02:20 +0300

    Small changes.

--- samples/GodotSample/Pies/Processing/View.cs
+++ samples/GodotSample/Pies/Processing/View.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
@@ -10,63 +10,63 @@ namespace GodotSample.Pies.Processing;
 
 public partial class View : ViewBase
 {
-    public View()
-    {
-        var viewModel = new ViewModel();
+	public View()
+	{
+		var viewModel = new ViewModel();
 
-        var hBoxContainer = new HBoxContainer
-        {
-            LayoutMode = 1,
-            AnchorsPreset = (int)LayoutPreset.FullRect
-        };
-        AddChild(hBoxContainer);
+		var hBoxContainer = new HBoxContainer
+		{
+			LayoutMode = 1,
+			AnchorsPreset = (int)LayoutPreset.FullRect
+		};
+		AddChild(hBoxContainer);
 
-        var labels = new VBoxContainer
-        {
-            SizeFlagsHorizontal = SizeFlags.ShrinkCenter,
-            SizeFlagsVertical = SizeFlags.ShrinkCenter
-        };
+		var labels = new VBoxContainer
+		{
+			SizeFlagsHorizontal = SizeFlags.ShrinkCenter,
+			SizeFlagsVertical = SizeFlags.ShrinkCenter
+		};
 
-        var label1 = new Label();
-        labels.AddChild(label1);
+		var label1 = new Label();
+		labels.AddChild(label1);
 
-        var label2 = new Label();
-        labels.AddChild(label2);
+		var label2 = new Label();
+		labels.AddChild(label2);
 
-        var label3 = new Label();
-        labels.AddChild(label3);
+		var label3 = new Label();
+		labels.AddChild(label3);
 
-        var pieChart = new PieChart
-        {
-            Series = viewModel.Series,
-            SizeFlagsStretchRatio = 3
-        };
-        pieChart.UpdateStarted += _ =>
-        {
-            var series = (PieSeries<ObservableValue>)viewModel.Series[0];
-            var values = (ObservableValue[])series.Values;
-            label1.Text = values[0].Value + " " + series.Name;
-            label1.SelfModulate = GetForeColor(series);
+		var pieChart = new PieChart
+		{
+			Series = viewModel.Series,
+			SizeFlagsStretchRatio = 3
+		};
+		pieChart.UpdateStarted += _ =>
+		{
+			var series = (PieSeries<ObservableValue>)viewModel.Series[0];
+			var values = (ObservableValue[])series.Values;
+			label1.Text = values[0].Value + " " + series.Name;
+			label1.SelfModulate = GetForeColor(series);
 
-            series = (PieSeries<ObservableValue>)viewModel.Series[1];
-            values = (ObservableValue[])series.Values;
-            label2.Text = values[0].Value + " " + series.Name;
-            label2.SelfModulate = GetForeColor(series);
+			series = (PieSeries<ObservableValue>)viewModel.Series[1];
+			values = (ObservableValue[])series.Values;
+			label2.Text = values[0].Value + " " + series.Name;
+			label2.SelfModulate = GetForeColor(series);
 
-            series = (PieSeries<ObservableValue>)viewModel.Series[2];
-            values = (ObservableValue[])series.Values;
-            label3.Text = values[0].Value + " " + series.Name;
-            label3.SelfModulate = GetForeColor(series);
-        };
+			series = (PieSeries<ObservableValue>)viewModel.Series[2];
+			values = (ObservableValue[])series.Values;
+			label3.Text = values[0].Value + " " + series.Name;
+			label3.SelfModulate = GetForeColor(series);
+		};
 
-        hBoxContainer.AddChild(pieChart);
-        hBoxContainer.AddChild(labels);
-    }
+		hBoxContainer.AddChild(pieChart);
+		hBoxContainer.AddChild(labels);
+	}
 
-    private static Color GetForeColor(PieSeries<ObservableValue> pieSeries)
-    {
-        return pieSeries.Fill is not SolidColorPaint solidColorBrush
-            ? new Color()
-            : solidColorBrush.Color.ToGDColor();
-    }
+	private static Color GetForeColor(PieSeries<ObservableValue> pieSeries)
+	{
+		return pieSeries.Fill is not SolidColorPaint solidColorBrush
+			? new Color()
+			: solidColorBrush.Color.ToGDColor();
+	}
 }
--- samples/GodotSample/Test/MotionCanvasDispose/View.cs
+++ samples/GodotSample/Test/MotionCanvasDispose/View.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using LiveChartsCore.SkiaSharpView.Godot;
 using ViewModelsSamples.Test.MotionCanvasDispose;
 
@@ -6,40 +6,40 @@ namespace GodotSample.Test.MotionCanvasDispose;
 
 public partial class View : VBoxViewBase
 {
-    public View()
-    {
-        var contentControl = new Control
-        {
-            LayoutMode = 1,
-            AnchorsPreset = (int)LayoutPreset.FullRect,
+	public View()
+	{
+		var contentControl = new Control
+		{
+			LayoutMode = 1,
+			AnchorsPreset = (int)LayoutPreset.FullRect,
 
-            SizeFlagsHorizontal = SizeFlags.ExpandFill,
-            SizeFlagsVertical = SizeFlags.ExpandFill
-        };
-        contentControl.AddChild(CreateContent());
+			SizeFlagsHorizontal = SizeFlags.ExpandFill,
+			SizeFlagsVertical = SizeFlags.ExpandFill
+		};
+		contentControl.AddChild(CreateContent());
 
-        var changeContentButton = new Button() { Text = "Change content" };
-        changeContentButton.Pressed += () =>
-        {
-            contentControl.RemoveChild(contentControl.GetChild(0));
-            contentControl.AddChild(CreateContent());
-        };
-        AddChild(changeContentButton);
+		var changeContentButton = new Button() { Text = "Change content" };
+		changeContentButton.Pressed += () =>
+		{
+			contentControl.RemoveChild(contentControl.GetChild(0));
+			contentControl.AddChild(CreateContent());
+		};
+		AddChild(changeContentButton);
 
-        AddChild(contentControl);
-    }
+		AddChild(contentControl);
+	}
 
-    private static Control CreateContent()
-    {
-        var canvas = new MotionCanvas
-        {
-            LayoutMode = 1,
-            AnchorsPreset = (int)LayoutPreset.FullRect,
+	private static Control CreateContent()
+	{
+		var canvas = new MotionCanvas
+		{
+			LayoutMode = 1,
+			AnchorsPreset = (int)LayoutPreset.FullRect,
 
-            SizeFlagsHorizontal = SizeFlags.ExpandFill,
-            SizeFlagsVertical = SizeFlags.ExpandFill
-        };
-        canvas.Ready += () => ViewModel.Generate(canvas.CoreCanvas);
-        return canvas;
-    }
+			SizeFlagsHorizontal = SizeFlags.ExpandFill,
+			SizeFlagsVertical = SizeFlags.ExpandFill
+		};
+		canvas.Ready += () => ViewModel.Generate(canvas.CoreCanvas);
+		return canvas;
+	}
 }
